36947d418a04edc170c8bb16b0ba8d8c84b979de
leopheard <leopheard.misc@gmail.com>
2019-09-19 04:00:01 -0400

    upload

--- addon.py
+++ addon.py
@@ -4,54 +4,50 @@ from resources.lib import mainaddon
 plugin = Plugin()
 
 # base url for fetching podcasts 
-URL = "http://feeds.revealradio.org/revealpodcast"
+URL1 = "http://feeds.revealradio.org/revealpodcast"
+URL2 = "https://www.revealnews.org/feed/"
+
+media:thumbnail
+url
 
 @plugin.route('/')
 def main_menu():
-    """
-    main menu 
-    """
     items = [
    {
             'label': plugin.get_string(30001), 
-            'path': plugin.url_for('all_episodes1'),
-            'thumbnail': "/home/osmc/.kodi/addons/plugin.audio.NPRpodcasts/resources/media/reveal300px.png"},
+            'path': plugin.url_for('episodes1'),
+            'thumbnail': "https://www.revealnews.org/wp-content/uploads/2017/11/reveal-square-logo-black-on-transparent-150x150.png"},
         {
             'label': plugin.get_string(30000), 
-            'path': plugin.url_for('all_episodes'),
-            'thumbnail': "/home/osmc/.kodi/addons/plugin.audio.NPRpodcasts/resources/media/reveal300px.png"},
+            'path': plugin.url_for('episodes'),
+            'thumbnail': "https://www.revealnews.org/wp-content/uploads/2017/11/reveal-square-logo-black-on-transparent-150x150.png"},
+   {
+            'label': plugin.get_string(30002), 
+            'path': plugin.url_for('episodes2'),
+            'thumbnail': "https://www.revealnews.org/wp-content/uploads/2017/11/reveal-square-logo-black-on-transparent-150x150.png"},
     ]
-
     return items
 
-
-@plugin.route('/all_episodes/')
+@plugin.route('/episodes/')
 def all_episodes():
-    """
-    contains playable podcasts listed as just-in
-    """
-    soup = mainaddon.get_soup(URL)
-    
-    playable_podcast = mainaddon.get_playable_podcast(soup)
-    
+    soup1 = mainaddon.get_soup1(URL1)
+    playable_podcast = mainaddon.get_playable_podcast(soup1)
     items = mainaddon.compile_playable_podcast(playable_podcast)
-
     return items
 
-
-@plugin.route('/all_episodes1/')
+@plugin.route('/episodes1/')
 def all_episodes1():
-    """
-    contains playable podcasts listed as just-in
-    """
-    soup = mainaddon.get_soup(URL)
-    
-    playable_podcast1 = mainaddon.get_playable_podcast1(soup)
-    
+    soup1 = mainaddon.get_soup1(URL1)
+    playable_podcast1 = mainaddon.get_playable_podcast1(soup1)
     items = mainaddon.compile_playable_podcast1(playable_podcast1)
-
     return items
 
+@plugin.route('/episodes2/')
+def all_episodes2():
+    soup2 = mainaddon.get_soup2(URL2)
+    playable_podcast2 = mainaddon.get_playable_podcast2(soup2)
+    items = mainaddon.compile_playable_podcast2(playable_podcast2)
+    return items
 
 if __name__ == '__main__':
     plugin.run()
--- resources/lib/mainaddon.py
+++ resources/lib/mainaddon.py
@@ -2,129 +2,108 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
-def get_soup(url):
-    """
-    @param: url of site to be scraped
-    """
-    page = requests.get(url)
-    soup = BeautifulSoup(page.text, 'html.parser')
+def get_soup1(url1):
+    page = requests.get(url1)
+    soup1 = BeautifulSoup(page.text, 'html.parser')
+    print("type: ", type(soup1))
+    return soup1
+get_soup1("http://feeds.revealradio.org/revealpodcast")
+def get_soup2(url2):
+    page = requests.get(url2)
+    soup2 = BeautifulSoup(page.text, 'html.parser')
+    print("type: ", type(soup2))
+    return soup2
+get_soup2("https://www.revealnews.org/feed/")
 
-    print "type: ", type(soup)
-    return soup
-
-get_soup("http://feeds.revealradio.org/revealpodcast")
-
-
-def get_playable_podcast(soup):
-    """
-    @param: parsed html page            
-    """
+def get_playable_podcast(soup1):
     subjects = []
-
-    for content in soup.find_all('item'):
+    for content in soup1.find_all('item'):
         try:        
             link = content.find('enclosure')
             link = link.get('url')
-            print "\n\nLink: ", link
-
+            print("\n\nLink: ", link)
             title = content.find('title')
             title = title.get_text()
-
-#            desc = content.find('itunes:subtitle')
-#            desc = desc.get_text()
-
             thumbnail = content.find('image')
             thumbnail = title.get_text('url')
-
-#            episode = content.find('itunes:episode')
-#            episode = title.get_text()
-
         except AttributeError:
             continue
-              
         item = {
                 'url': link,
                 'title': title,
-#                'desc': desc,
                 'thumbnail': thumbnail,
-#                'episode': episode,
         }
-        
-        subjects.append(item) 
-    
+        subjects.append(item)
     return subjects
-
-
 def compile_playable_podcast(playable_podcast):
-    """
-    @para: list containing dict of key/values pairs for playable podcasts
-    """
     items = []
-
     for podcast in playable_podcast:
         items.append({
             'label': podcast['title'],
             'thumbnail': podcast['thumbnail'],
             'path': podcast['url'],
-            'info': podcast['desc'],
             'is_playable': True,
     })
-
     return items
 
-
-def get_playable_podcast1(soup):
-    """
-    @param: parsed html page            
-    """
+def get_playable_podcast1(soup1):
     subjects = []
-
-    for content in soup.find_all('item', limit=5):
-        try:        
+    for content in soup1.find_all('item', limit=9):
+        try:
             link = content.find('enclosure')
             link = link.get('url')
-            print "\n\nLink: ", link
-
+            print("\n\nLink: ", link)
             title = content.find('title')
             title = title.get_text()
-
-#            desc = content.find('itunes:subtitle')
-#            desc = desc.get_text()
-
             thumbnail = content.find('image')
             thumbnail = title.get_text('url')
-
-#            episode = content.find('itunes:episode')
-#            episode = title.get_text()
-
         except AttributeError:
             continue
-              
         item = {
                 'url': link,
                 'title': title,
-#                'desc': desc,
                 'thumbnail': thumbnail,
-#                'episode': episode,
         }
-        
         subjects.append(item) 
     return subjects
-
-
 def compile_playable_podcast1(playable_podcast1):
-    """
-    @para: list containing dict of key/values pairs for playable podcasts
-    """
     items = []
-
     for podcast in playable_podcast1:
         items.append({
             'label': podcast['title'],
             'thumbnail': podcast['thumbnail'],
             'path': podcast['url'],
-            'info': podcast['desc'],
             'is_playable': True,
     })
+    return items
 
+def get_playable_podcast2(soup2):
+    subjects = []
+    for content in soup2.find_all('item'):
+        try:
+            link = content.find('source')
+            link = link.get('src')
+            print("\n\nLink: ", link)
+            title = content.find('title')
+            title = title.get_text()
+            thumbnail = content.find('media:thumbnail')
+            thumbnail = title.get_text('url')
+        except AttributeError:
+            continue
+        item = {
+                'url': link,
+                'title': title,
+                'thumbnail': thumbnail,
+        }
+        subjects.append(item) 
+    return subjects
+def compile_playable_podcast2(playable_podcast2):
+    items = []
+    for podcast in playable_podcast2:
+        items.append({
+            'label': podcast['title'],
+            'thumbnail': podcast['thumbnail'],
+            'path': podcast['url'],
+            'is_playable': True,
+    })
     return items
